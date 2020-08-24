8b93f2906db62f4bcf43e2bfbedae9b2693352d0
Julia Bauer <julia.jjbauer@gmail.com>
2020-08-24 15:39:53 -0300

    add airport class add 4 airports and 1 flight

--- airline/flights/models.py
+++ airline/flights/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    #origin = models.CharField(max_length=64)
+    #on_delete=CASCADE --> se um aeroporto for deletado, todos os voo dele também são
+    #related name --> forma de acessar uma relação em uma ordem reversa
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+
+    #destination = models.CharField(max_length=64)
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+
     duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
