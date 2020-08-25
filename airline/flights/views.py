d75bc5afaa417227bbbddf595d158a877770f7fb
Julia Bauer <julia.jjbauer@gmail.com>
2020-08-25 15:45:37 -0300

    add flight.html

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -7,3 +7,10 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+#pk = primarykey
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight
+    })
